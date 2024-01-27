7ee7a8abfc705b4a707462a090f7c5cc623ba1a9
syedbadhusha <syedbadhusha21@gmail.com>
2024-01-27 12:42:17 +0530

    lodash added in equal function

--- script.js
+++ script.js
@@ -3,4 +3,4 @@
 let obj1 = {name:"person1",age:5}
 let obj2 = {age:5,name:"person1"}
 console.log( JSON.stringify(obj1)=== JSON.stringify(obj2) );
-console.log(isEqual(obj1, obj2) );
+console.log(_.isEqual(obj1, obj2) );
